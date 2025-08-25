7dc84399ee02f4c598b172011fa346ec31849f12
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-25 09:10:58 +0600

    the working page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google"
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <GoogleOAuthProvider clientId={GooglePublicApi ?? ""}>
           {children}
+          <Toaster />
         </GoogleOAuthProvider>
       </body>
     </html>
